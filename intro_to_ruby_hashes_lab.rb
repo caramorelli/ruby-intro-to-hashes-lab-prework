c0735211caed7dd868962891c9ae4b356aa6fbe8
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 01:15:09 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def new_hash
-
+  
 end
 
 def actor
@@ -12,7 +12,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly_hash = monopoly
 	
 end
 
