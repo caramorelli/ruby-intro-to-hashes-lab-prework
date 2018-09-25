2d9e1cb0078201f87880eb0612910b8bc1e21d80
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 01:30:35 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,8 @@ def monopoly_with_second_tier
 	  :railroads => 
 	    { 
 	      :pieces => 4, 
-	      :names => 
-	        { :reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {} }, :rent_in_dollars =>  {} }
+	      :names => {}, 
+	      :rent_in_dollars => {}  } }
 	
 end
 
